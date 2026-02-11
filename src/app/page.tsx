3077105c2de188d34091fe4d5d03762f1b46bf47
aditya-pushkar <adityapushkar850@gmail.com>
2026-02-12 03:58:57 +0530

    dummy content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
 	return (
 		<div>
-			<h1 className="text-4xl tracking-tight font-semibold bg-black">Hello Daddy</h1>
+			<h1 className="text-4xl tracking-tight font-semibold text-pink-600">Agent Daddy</h1>
 		<p className="text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus quo est alias, culpa sed odio, voluptates numquam voluptatum perferendis soluta obcaecati repudiandae odit, perspiciatis vel possimus dignissimos repellendus ratione consequuntur?</p>
 		<Button variant={"secondary"} size={"xs"} className="ring-2 ring-primary">Deploy Claw Now</Button>
 		</div>
