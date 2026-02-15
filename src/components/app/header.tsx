e6ec5c6940f6c9227d9ecb0ca795b862f5623c2b
aditya-pushkar <adityapushkar850@gmail.com>
2026-02-16 02:44:42 +0530

    rmd unused var

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Link from "next/link";
-
 // import Image from "next/image";
 import { UserProfile } from "./user-profile";
 import { ConnectDigitalOcean } from "./connect-digital-ocean";
