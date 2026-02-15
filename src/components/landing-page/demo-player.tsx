143be7dd054f4251ad8d86aade5d1755fa45e729
aditya-pushkar <adityapushkar850@gmail.com>
2026-02-16 04:17:04 +0530

    demo video and thumbnail added

--- src/components/landing-page/demo-player.tsx
+++ src/components/landing-page/demo-player.tsx
@@ -5,8 +5,8 @@ import { VideoPlayer } from "../ui/video-player";
 export function DemoVideoPlayerDemo() {
   return (
     <VideoPlayer
-      src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-      poster="https://peach.blender.org/wp-content/uploads/bbb-splash.png"
+      src="https://bucket.agentdaddie.com/agentdaddie.mov"
+      poster="/thumbnail.png"
       size="default"
       className="aspect-video border rounded-2xl"
     />
